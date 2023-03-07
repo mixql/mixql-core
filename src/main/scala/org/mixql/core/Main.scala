b630735d352a92a1b1910e2a7429aee6b5433ef0
Михаил Кривенко <mihan1235@yandex.ru>
2023-03-07 19:10:24 +0300

    Moved code from main function, so it can be used externaly

--- src/main/scala/org/mixql/core/Main.scala
+++ src/main/scala/org/mixql/core/Main.scala
@@ -26,46 +26,47 @@ object Main {
     override def isParam(name: String): Boolean = true
   }
 
+  val code =
+    """some code;;
+      |let gg = 12.4 - 11.2;
+      |let wp.x = $gg > 11;
+      |let res = 'one' + 'two';
+      |let check_case = case when 1 > 2 then 12 when 1 < 2 then 13 else '12g' end;
+      |if 12 < 11 then
+      |  print(true);
+      |else
+      |  print(false);
+      |end if
+      |let x = 0;
+      |while $x < 5 do
+      |  print($x);
+      |  let x = $x + 1;
+      |end while
+      |for i in 1..20 step 2 loop
+      |  print($i);
+      |end loop
+      |select '${$gg + 2}' from wp;
+      |select '${$gg * 5}' from wp;
+      |print("\$res");
+      |print("$res");
+      |print("${$res}");
+      |print("${'${'$res'+'a'}'}\"");
+      |let t="${'1'+$res}";
+      |let t="123;
+      |   \n 456";
+      |print($t);
+      |let t="${'1'+ '${$res}'}";
+      |print($t);
+      |some end;
+      |print(current_timestamp);
+      |let arr = [3, 'gg'];
+      |let arr[0] = 4;
+      |print($arr[0]);
+      |let mapa = {1: 1, "1": 2};
+      |print($mapa[1]);
+      |print($mapa["1"]);""".stripMargin
+
   def main(args: Array[String]): Unit = {
-    val code =
-      """some code;;
-        |let gg = 12.4 - 11.2;
-        |let wp.x = $gg > 11;
-        |let res = 'one' + 'two';
-        |let check_case = case when 1 > 2 then 12 when 1 < 2 then 13 else '12g' end;
-        |if 12 < 11 then
-        |  print(true);
-        |else
-        |  print(false);
-        |end if
-        |let x = 0;
-        |while $x < 5 do
-        |  print($x);
-        |  let x = $x + 1;
-        |end while
-        |for i in 1..20 step 2 loop
-        |  print($i);
-        |end loop
-        |select '${$gg + 2}' from wp;
-        |select '${$gg * 5}' from wp;
-        |print("\$res");
-        |print("$res");
-        |print("${$res}");
-        |print("${'${'$res'+'a'}'}\"");
-        |let t="${'1'+$res}";
-        |let t="123;
-        |   \n 456";
-        |print($t);
-        |let t="${'1'+ '${$res}'}";
-        |print($t);
-        |some end;
-        |print(current_timestamp);
-        |let arr = [3, 'gg'];
-        |let arr[0] = 4;
-        |print($arr[0]);
-        |let mapa = {1: 1, "1": 2};
-        |print($mapa[1]);
-        |print($mapa["1"]);""".stripMargin
     val context =
       new Context(MutMap[String, Engine]("demo" -> new DemoEngine), "demo")
     val res = core.run(code, context)
