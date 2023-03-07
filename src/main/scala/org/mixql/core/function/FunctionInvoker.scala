2e7723402dce3c9486f48847a94309df76685f20
Михаил Кривенко <mihan1235@yandex.ru>
2023-03-07 10:24:26 +0300

    Use one general functionInvoker for all scala versions

--- src/main/scala/org/mixql/core/function/FunctionInvoker.scala
+++ src/main/scala/org/mixql/core/function/FunctionInvoker.scala
@@ -1,12 +1,11 @@
 package org.mixql.core.function
 
 import org.mixql.core.context.Context
-import org.mixql.core.context.gtype._
-import java.lang.reflect.Method
-import scala.collection.mutable.ListBuffer
+import org.mixql.core.context.gtype.*
 
-import scala.reflect.runtime.{universe => ru}
+import java.lang.reflect.Method
 import scala.annotation.meta.param
+import scala.collection.mutable.ListBuffer
 
 object FunctionInvoker {
   def invoke(
@@ -53,24 +52,19 @@ object FunctionInvoker {
         }
       case None =>
         if (kwargs.nonEmpty)
-          throw new UnsupportedOperationException(
-            "named args for engine function not supported"
-          )
-        if (
-          context.currentEngine.getDefinedFunctions.contains(
-            funcName.toLowerCase
-          )
+        throw new UnsupportedOperationException(
+          "named args for engine function not supported"
         )
-          unpack(
-            context.currentEngine.executeFunc(funcName, args.map(pack): _*)
-          )
+        if (
+          context.currentEngine.getDefinedFunctions.contains(funcName.toLowerCase)
+        )
+          unpack(context.currentEngine.executeFunc(funcName, args.map(pack): _*))
         else {
           val engine = context.engines.find(eng =>
             eng._2.getDefinedFunctions.contains(funcName)
           )
           engine match {
-            case Some(value) =>
-              unpack(value._2.executeFunc(funcName, args.map(pack): _*))
+            case Some(value) => unpack(value._2.executeFunc(funcName, args.map(pack): _*))
             case None =>
               throw new NoSuchMethodException(
                 s"no function $funcName found for any engine"
@@ -121,26 +115,32 @@ object FunctionInvoker {
   ): Any = {
     if (obj.isInstanceOf[SqlLambda])
       return obj.asInstanceOf[SqlLambda].apply(args: _*)
-    val a = obj.getClass.getMethods.find(_.getName == "apply")
+    val a = obj.getClass.getMethods.find(p =>
+      p.getName == "apply" && (p.getParameters.length == 0 || p
+        .getParameters()(0)
+        .getName.toLowerCase != "v1")
+    )
     a match {
       case Some(apply) =>
-        val mirror = ru.runtimeMirror(this.getClass.getClassLoader)
-        val cl = mirror.classSymbol(obj.getClass).toType
-        val applyMethod =
-          cl.member(ru.TermName("apply")).asTerm.alternatives.head.asMethod
-        val applyParams = applyMethod.paramLists.head
+        val applyParams = apply.getParameters
         var lb: ListBuffer[Object] = ListBuffer()
         var args1 = args
         var kwargs1 = kwargs
         var i = 1
         val size = applyParams.length
         applyParams.foreach(param => {
-          val pname = param.asTerm.name.toString
-          val ptype = param.asTerm.typeSignature.baseClasses.head
+          val pname = param.getName
+          val ptype = param.getType
+          val cc = "org.mixql.core.context.Context"
+          val seqc = "scala.collection.immutable.Seq"
+          val seqcOld = "scala.collection.Seq" //In case of scala 2.12
           // argument is variable number of args like gg: String*
-          if (i == size && ptype == ru.typeOf[Seq[Object]].baseClasses.head) {
+          if (i == size && (ptype.getName == seqc) || (ptype.getName == seqcOld)) {
             lb += args1
-          } else if (param.asTerm.typeSignature == ru.typeOf[Context]) {
+          } else if (
+            ptype.getName == cc ||
+            ptype == Context.getClass
+          ) {
             lb += context
           } else if (kwargs1.contains(pname)) {
             lb += kwargs1(pname)
@@ -163,6 +163,7 @@ object FunctionInvoker {
 
   private def getDefParamsFor(obj: Object, i: Int): Object = {
     val paramName = s"apply$$default$$$i"
-    obj.getClass.getMethod(paramName).invoke(obj)
+    val defval = obj.getClass.getMethod(paramName)
+    defval.invoke(obj)
   }
 }
