cbf207dfc0e9fcb8f7445641dc10de7b5935349a
mihan1235 <mihan1235@yandex.ru>
2023-07-19 21:52:13 +0300

    Added some more functions for context with shared vars in engine

--- src/main/scala/org/mixql/core/context/ContextVars.scala
+++ src/main/scala/org/mixql/core/context/ContextVars.scala
@@ -2,8 +2,22 @@ package org.mixql.core.context
 
 import org.mixql.core.context.gtype.Type
 
-class ContextVars(val context: Context){
+class ContextVars(val context: Context) {
   def setVar(key: String, value: Type): Unit = context.setVar(key, value)
+
   def getVar(key: String): Type = context.getVar(key)
 
+  def getVars(keys: List[String]): List[Type] = {
+    var res: List[Type] = List()
+    keys.foreach(key => res = res :+ context.getVar(key))
+    res
+  }
+
+  def setVars(vars: Map[String, Type]): Unit = {
+    vars.foreach(variable => context.setVar(variable._1, variable._2))
+  }
+
+  def getVarsNames(): List[String] = {
+    context.getScope().flatMap(scope => scope.keys.toList)
+  }
 }
