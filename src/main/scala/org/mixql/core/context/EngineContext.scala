279463763e05b00f2fb6eeb4899ba7a8ca642cc3
mihan1235 <mihan1235@yandex.ru>
2023-08-03 15:15:08 +0300

    #30 Added invoke of function in engine's context

--- src/main/scala/org/mixql/core/context/EngineContext.scala
+++ src/main/scala/org/mixql/core/context/EngineContext.scala
@@ -1,6 +1,7 @@
 package org.mixql.core.context
 
-import org.mixql.core.context.gtype.Type
+import org.mixql.core.context.gtype.{Type, pack}
+import org.mixql.core.function.FunctionInvoker
 
 import scala.collection.mutable
 
@@ -25,5 +26,33 @@ class EngineContext(val context: Context) {
 
   def getVarsNames(): List[String] = {
     context.getScope().flatMap(scope => scope.keys.toList)
+  }
+
+  /** invoke function using context, can call also functions from other engines
+    * and default context functions
+    *
+    * @param funcName
+    *   name of function
+    * @param args
+    *   arguments for function
+    * @param kwargs
+    *   named arguments for function
+    */
+  def invokeFunction(funcName: String, args: List[Any] = Nil, kwargs: Map[String, Object] = Map.empty): Type = {
+    try {
+      val res = FunctionInvoker.invoke(Map(context.functions.toSeq: _*), funcName, context, args, kwargs)
+      pack(res)
+    } catch {
+      case e: java.lang.reflect.InvocationTargetException =>
+        val errorMsg =
+          "Error while invoke of " + funcName + " \n" +
+            "Error: " + e.getClass.getName + " " + "msg: \n" +
+            e.getMessage + "\n" +
+            "target exception: " + e.getTargetException.getClass.getName + "\n" +
+            "target exception msg: \n" + e.getTargetException.getMessage + "\n" +
+            "target exception stacktrace: \n" + e.getTargetException.getStackTraceString
+        throw new Exception(errorMsg)
+      case e: Exception => throw e
+    }
   }
 }
